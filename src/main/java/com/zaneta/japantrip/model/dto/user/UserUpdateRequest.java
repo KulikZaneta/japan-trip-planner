d8170d88783e741a45246d8561b82d67ce61015c
Zaneta <kul.zaneta@gmail.com>
2025-09-25 10:17:13 +0200

    add put/patch user dto

--- src/main/java/com/zaneta/japantrip/model/dto/user/UserUpdateRequest.java
+++ src/main/java/com/zaneta/japantrip/model/dto/user/UserUpdateRequest.java
@@ -1,37 +1,31 @@
 package com.zaneta.japantrip.model.dto.user;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class UserUpdateRequest {
+public record UserUpdateRequest(@NotBlank(message = "First name is required")
+                                @Size(max = 50, message = "First name must be at most 50 characters")
+                                String firstName,
 
-    @Size(max = 50,message = "FirstName must be at most 50 characters")
-    private String firstName;
+                                @NotBlank(message = "Last name is required")
+                                @Size(max = 50, message = "Last name must be at most 50 characters")
+                                String lastName,
 
-    @Size(max = 50,message = "LastName must be at most 50 characters")
-    private String lastName;
+                                @NotBlank(message = "Nickname is required")
+                                @Size(max = 50, message = "Nickname must be at most 50 characters")
+                                String nickname,
 
-    @Size(max = 50,message = "Nickname must be at most 50 characters")
-    private String nickname;
+                                @NotBlank(message = "Email is required")
+                                @Email(message = "Email must be valid")
+                                String email,
 
-    @Email(message = "Email must be valid")
-    private String email;
-
-    @Size(min = 8, message = "Password must be at least 8 characters")
-    @Pattern(
-            regexp = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-={}\\[\\]:;\"'|,.<>/?]).{8,}$",
-            message = "Password must contain at least 1 number, 1 uppercase letter and 1 special character"
-    )
-    private String password;
-
+                                @NotBlank(message = "Password is required")
+                                @Size(min = 8, message = "Password must be at least 8 characters")
+                                @Pattern(
+                                        regexp = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-={}\\[\\]:;\"'|,.<>/?]).{8,}$",
+                                        message = "Password must contain at least 1 number, 1 uppercase letter and 1 special character"
+                                )
+                                String password) {
 }
-
